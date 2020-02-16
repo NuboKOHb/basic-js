2c3dd3af47564922c050cc8ff9c9af70cee306bb
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-17 01:36:08 +0300

    fix: solution Count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,11 @@
 module.exports = function countCats(matrix) {
   let countCats = 0;
-  for (let i=0; i < matrix.length ;i++) {
-    countCats+= matrix[i].join("").split("^^").length;
+  for (let i = 0; i < matrix.length ;i++) {
+    for(let j = 0; j < matrix[i].length; j++ ) {
+      if (matrix[i][j] == "^^") {
+        countCats++;
+      }
+    }
   }
   return countCats;
 };
