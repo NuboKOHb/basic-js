6f8754aaf10608c1ac3e90681655d8bbfd5524e9
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-18 01:29:58 +0300

    add: basic solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason(date = new Date) {
+  let season =["winter", "spring", "summer", "autumn"];
+  if (date.getMonth() in [11,0,1]) {
+    console.log(season[0]);
+    return season[0];
+  } else if (date.getMonth in [2,3,4]) {
+    return season[1];
+  } else if (date.getMonth in [5,6,7]) {
+    return season[2];
+  } else if (date.getMonth in [8,9,10]) {
+    return season[3];
+  } else {
+    return false;
+  }
+
+
+  //  throw 'Not implemented';
   // remove line with error and write your code here
 };
