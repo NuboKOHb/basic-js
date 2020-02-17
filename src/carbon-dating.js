c16f9bc9e58707b09a202ed8ad9f9cf4883a994e
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-17 03:18:18 +0300

    fix: solution carbon test

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,18 +2,27 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+
   if (typeof(sampleActivity) != 'string') {
     return false;
+  }
+  if (sampleActivity.split(".").length > 2) {
+    sampleActivity = sampleActivity.split(".");
+    sampleActivity.pop();
+    console.log(sampleActivity);
+    sampleActivity=sampleActivity.join(".");
+    console.log(sampleActivity);
   }
   if (isNaN(+sampleActivity)) {
     return false;
   }
   if ((+sampleActivity) <= 0 ) {
     return false;
-  } 
+  }
   if ((+sampleActivity) > 15) {
     return false;
   } 
+
    
   let k = (0.693/HALF_LIFE_PERIOD);
   let year = Math.ceil(Math.log(MODERN_ACTIVITY/+sampleActivity)/k);
@@ -23,4 +32,4 @@ module.exports = function dateSample(sampleActivity) {
   return year;
 };
 
-//console.log(dateSample('3'), 13308);
+//console.log(dateSample('11.3231.3213124'), 2326);
