be58f16cca84fb5044cdd2856d770435761e2f8e
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-18 02:13:26 +0300

    fix: solution test

--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,18 @@
-module.exports = function getSeason(date = new Date) {
-  let season =["winter", "spring", "summer", "autumn"];
-  if (date.getMonth() in [11,0,1]) {
-    console.log(season[0]);
-    return season[0];
-  } else if (date.getMonth in [2,3,4]) {
-    return season[1];
-  } else if (date.getMonth in [5,6,7]) {
-    return season[2];
-  } else if (date.getMonth in [8,9,10]) {
-    return season[3];
-  } else {
-    return false;
+module.exports = function getSeason(date) {
+  
+  if (!date) {
+    return 'Unable to determine the time of year!';
   }
 
-
-  //  throw 'Not implemented';
+  let month = (date.getMonth())?date.getMonth():-1;
+  
+  if (month && month >=0 && month <= 11) {
+    let season =["winter", "spring", "summer", "autumn"];
+    return season[Math.floor([11,0,1,2,3,4,5,6,7,8,9,10].indexOf(month)/3)];
+  } else {
+     throw new Error;
+  }
   // remove line with error and write your code here
 };
+
+//console.log(getSeason(new Date(2361, 2, 24, 22, 13, 47, 69)), 'spring');
