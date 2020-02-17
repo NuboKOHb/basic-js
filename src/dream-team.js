6387fb7b2216d31cbf7ed945f473e6df13f98c85
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-17 03:40:36 +0300

    add: solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let res = "";
+  if (arr == null) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) == 'string') {
+      arr[i] = arr[i].trim().toUpperCase();
+      if(/^[a-zA-Z]$/.test(arr[i].slice(0,1))){
+        res += arr[i].slice(0,1);
+      }
+    }
+  }
+  if (res == "") {
+    return false;
+  } else {
+    return res.split('').sort().join('');
+  }
 };
+
