c84eb8790657581eeb3047e756a84ffe5e40c9c8
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-18 19:08:07 +0300

    feat: all tests is pass

--- src/what-season.js
+++ src/what-season.js
@@ -3,16 +3,19 @@ module.exports = function getSeason(date) {
   if (!date) {
     return 'Unable to determine the time of year!';
   }
+  if (!Date.prototype.isPrototypeOf(date)) {
+    throw new Error('lie');
+  }
+  if (!(date.toString()).includes(date.getFullYear())) {
+    throw new Error('lie');
+  }
 
-  let month = (date.getMonth())?date.getMonth():-1;
+  let month = date.getMonth();
   
-  if (month && month >=0 && month <= 11) {
+  if (month >=0 && month <= 11) {
     let season =["winter", "spring", "summer", "autumn"];
     return season[Math.floor([11,0,1,2,3,4,5,6,7,8,9,10].indexOf(month)/3)];
   } else {
      throw new Error;
   }
-  // remove line with error and write your code here
 };
-
-//console.log(getSeason(new Date(2361, 2, 24, 22, 13, 47, 69)), 'spring');
