9c960c28a943372f410748acbbb5bdaf3a718ca5
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-18 23:10:37 +0300

    feat: all tests is pass

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,9 +1,8 @@
-module.exports = 
-function transform(arr) {
+module.exports = function transform(arr) {
     if (!arr || [].__proto__ !== arr.__proto__) {
         throw new Error();
     }
-    function rr (arr) {
+    function rr(arr) {
         let result = [];
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] == '--discard-next') { 
@@ -22,21 +21,24 @@ function transform(arr) {
                 result.push(arr[i-1]);
                 continue;
             }
-            result.push(arr[i]); 
+            if(arr[i] !== undefined) {
+                result.push(arr[i]);
+            }
         }
         return result;
     }
-
-    while (arr.join(',').includes('--discard-next')||
-    arr.join(',').includes("--discard-prev")||
-    arr.join(',').includes('--double-next')||
-    arr.join(',').includes('--double-prev'))
+    do 
     {
         arr = rr(arr);
     }
+    while (arr.includes('--discard-next')||
+    arr.includes("--discard-prev")||
+    arr.includes('--double-next')||
+    arr.includes('--double-prev') || arr.includes(undefined))
+    //*/
     return arr;
 };
 
 //console.log(transform([1, 2, 3, '--double-next', 4, 5]));
 //console.log(transform([1, 2, 3, '--discard-prev', 4, 5]));
-//console.log([].__proto__);
+//console.log(transform(["--discard-next","DEF","--discard-next"]));
