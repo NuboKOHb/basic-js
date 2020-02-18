54187478f1290b6ed75b4149012f93593c8ed1d4
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-18 22:32:03 +0300

    feat: more tests is pass

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,42 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = 
+function transform(arr) {
+    if (!arr || [].__proto__ !== arr.__proto__) {
+        throw new Error();
+    }
+    function rr (arr) {
+        let result = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == '--discard-next') { 
+            i++;
+            continue;
+            }
+            if (arr[i] == '--discard-prev') { 
+                result.pop();
+                continue;
+            }
+            if (arr[i] == '--double-next') { 
+                result.push(arr[i+1]);
+                continue;
+            }
+            if (arr[i] == '--double-prev') { 
+                result.push(arr[i-1]);
+                continue;
+            }
+            result.push(arr[i]); 
+        }
+        return result;
+    }
+
+    while (arr.join(',').includes('--discard-next')||
+    arr.join(',').includes("--discard-prev")||
+    arr.join(',').includes('--double-next')||
+    arr.join(',').includes('--double-prev'))
+    {
+        arr = rr(arr);
+    }
+    return arr;
 };
+
+//console.log(transform([1, 2, 3, '--double-next', 4, 5]));
+//console.log(transform([1, 2, 3, '--discard-prev', 4, 5]));
+//console.log([].__proto__);
