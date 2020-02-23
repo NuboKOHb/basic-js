cd872151a0b3e6280983f7566fac0dff62dc10b0
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-24 01:17:49 +0300

    feat: all tests is pass

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,45 @@
 const chainMaker = {
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!this.chain) {this.chain = []}
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!this.chain) {this.chain = []}
+    if (value === undefined) {value = '';}
+    this.chain.push(value);
+    return this; 
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!this.chain) {this.chain = []}
+
+    if (typeof(position) !== 'number' || 
+    (position - Math.trunc(position) !== 0) ||
+    position < 1 || position > this.chain.length
+    ) {
+      this.chain = [];
+      throw new Error;
+    }
+
+    this.chain.splice(position-1,1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!this.chain) {this.chain = []}
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = '';
+    for (let i = 0; i < this.chain.length; i++ ) {
+      if (i == 0) {
+        result = `( ${this.chain[0]} )`;
+      } else {
+        result = result + `~~( ${this.chain[i]} )`;
+      }
+    }
+
+    this.chain = [];
+    return result;
   }
 };
-
 module.exports = chainMaker;
