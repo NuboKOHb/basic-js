861de890b118ed1b96471b7bda86a392fc79c61e
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-24 02:57:39 +0300

    feat: some tests is pass

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -4,15 +4,16 @@ module.exports = class DepthCalculator {
         if (!Array.isArray(arr)) {
             return 0;
         } else {
-            if(arr.length == 0) {
+            if(arr.length === 0) {
                 return 1;
+            } else {
+                for (let i = 0; i < arr.length; i++) {
+                    if (Array.isArray(arr[i])) {arr[i] = calculateDepth(arr[i]);}
+                    else {arr[i] = 0;}
+                }
+                arr.sort((a,b)=>(b-a));
+                return 1 + arr[0]; 
             }
-        for (let i = 0; i < arr.length; i++) {
-            arr[i] = calculateDepth(arr[i]);
-        }
-        console.log(arr);
-        arr.sort((a,b)=>(b-a));
-        return 1 + arr[0]; 
         }
     }
 };
